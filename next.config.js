84acbdef01679b54e9dd02f4597647ed1688076b
halilatilla <atillahalil0@gmail.com>
2020-04-24 17:34:57 +0300

    asdf

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 // next.config.js
-const withSass = require("@zeit/next-sass");
-require("dotenv").config();
+const withSass = require('@zeit/next-sass');
+require('dotenv').config();
 module.exports = withSass({
+  target: 'serverless',
   env: {
-    API_KEY: process.env.API_KEY,
-  },
+    API_KEY: process.env.API_KEY
+  }
 });
